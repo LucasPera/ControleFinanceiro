a304cba811307148c727b1ca2fac7493d00a7e07
LucasPera <lucasperamarchi@hotmail.com>
2025-03-25 23:04:29 -0300

    Initial commit

---
+++ controleficanceiro/src/main/java/br/com/controlefinanceiro/controleficanceiro/ControleficanceiroApplication.java
@@ -0,0 +1,13 @@
+package br.com.controlefinanceiro.controleficanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleficanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleficanceiroApplication.class, args);
+	}
+
+}
